85a8e0ec1100232f9f9fad8d94f8866ad305a211
[SW5] 조민호 <miinnnhho>
2023-07-20 15:00:11 +0900

    0720

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Button, Container } from '@mui/material';
+import { Button, Container, Grid } from '@mui/material';
 
 // @ts-ignore
 function Counter({ title, initValue }) {
-    const [count, setCount] = useState(0);
-    {
-        /* css inline 방식  */
+    const [count, setCount] = useState(initValue);
+    const [step, setStep] = useState(1);
+    function upHandler() {
+        setCount(count + step);
     }
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCount((oldCount:number)=> {return oldCount+1});
+    //     }, 1000)
+    // },[]);
     return (
         <div
             style={{
                 border: '1px solid black',
                 fontSize: 10,
+                padding: 10,
             }}
         >
             <h1>{title}</h1>
-            <Button variant="contained"
-                className="btn"
-                onClick={() => {
-                    setCount(count + 1);
-                }}
-            >
+            <Button variant="outlined" className="btn" onClick={upHandler}>
                 +
             </Button>{' '}
+            <input type='number' value={step} onChange={(evt)=> {
+                // @ts-ignore
+                setStep(Number(evt.target.value));
+            }}/>
             {count}
         </div>
     );
@@ -32,10 +38,15 @@ function Counter({ title, initValue }) {
 function App() {
     return (
         <Container maxWidth="md">
-            <Counter title="카운터" initValue={0} />
-            <Counter title="카운터" initValue={0} />
+            <Grid container spacing={1}>
+                <Grid item xs={12} sm={6} md={4}>
+                    <Counter title="카운터" initValue={0} />
+                </Grid>
+            </Grid>
         </Container>
     );
 }
 
 export default App;
+
+//spacing={1} 1 -> 8px
